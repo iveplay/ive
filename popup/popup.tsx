2ced2340c4cd400997234149a34589a0d0978f56
ive <contact@iveplay.io>
2025-04-30 02:04:51 +0200

    popup

--- popup/popup.tsx
+++ popup/popup.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PopupApp } from '@/pages/popup/PopupApp'
 import '@mantine/core/styles.css'
-import '@/styles/global.scss'
+import './global.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
