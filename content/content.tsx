874b28d061353c66431c7a935f0b6421792ce423
ive <contact@iveplay.io>
2025-05-11 02:57:04 +0200

    feat: improved component loading

--- content/content.tsx
+++ content/content.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { CSSProperties, ReactNode, StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { EroLoadPanel } from '@/pages/eroLoadPanel/EroLoadPanel'
 import { FaptapPanel } from '@/pages/faptapPanel/FaptapPanel'
@@ -11,83 +11,91 @@ const EROSCRIPT_URL = 'discuss.eroscripts.com/t/'
 const FAPTAP_URL = 'faptap.net/v'
 
 let currentUrl = window.location.href
+let mountedComponent = false
 
 // Expose extension API to the page
 setupIveEventApi()
 
 setInterval(() => {
   if (window.location.href !== currentUrl) {
-    const loadScriptPage = [EROSCRIPT_URL, FAPTAP_URL].find((page) =>
-      currentUrl.includes(page),
-    )
-
     currentUrl = window.location.href
-    if (!loadScriptPage) {
-      handleUrlChange()
-    }
+    mountedComponent = false
+    document.getElementById('ive')?.remove()
+    handleUrlChange()
+  } else if (!mountedComponent && window.location.href.includes(FAPTAP_URL)) {
+    handleUrlChange()
   }
 }, 1000)
 
-async function handleUrlChange() {
-  const scriptMappings = await getScripts()
+const handleUrlChange = async () => {
+  if (document.getElementById('ive')) {
+    // Prevent duplicate components
+    return
+  }
 
-  const scripts = Object.entries(scriptMappings).find(([url, scripts]) => {
-    if (currentUrl.includes(url)) {
-      return scripts
+  const scriptMappings = await getScripts()
+  const scripts = Object.entries(scriptMappings).find(([url]) =>
+    currentUrl.includes(url),
+  )?.[1]
+
+  try {
+    if (currentUrl.includes(EROSCRIPT_URL)) {
+      const container = document.getElementsByClassName(
+        'icons d-header-icons',
+      )[0]
+      if (container) {
+        mountComponent(container, <EroLoadPanel />, 'prepend', {
+          position: 'relative',
+        })
+      }
+    } else if (currentUrl.includes(FAPTAP_URL)) {
+      const container = await findHtmlElement(
+        '#app > div > div.py-2.lg\\:py-8.lg\\:px-8.flex-1.flex.flex-col.relative > div.flex-1 > div.text-white.flex.flex-col.gap-y-2.mt-3.px-2.md\\:px-0 > div.flex.flex-col-reverse.lg\\:flex-row.lg\\:items-center.justify-between.gap-y-2 > div.relative.-mx-2.lg\\:mx-0 > div > div',
+      )
+
+      if (container) {
+        mountComponent(container, <FaptapPanel />, 'prepend', {
+          position: 'relative',
+        })
+      }
+    } else if (scripts) {
+      mountComponent(document.body, <VideoPage scripts={scripts} />, 'append', {
+        zIndex: '2147483640',
+        position: 'fixed',
+        inset: '0',
+        pointerEvents: 'none',
+      })
     }
-    return undefined
+  } catch (error) {
+    console.error('Error mounting IVE component:', error)
+  }
+}
+
+const mountComponent = (
+  container: Element,
+  component: ReactNode,
+  insertMethod: 'append' | 'prepend' = 'append',
+  styles: CSSProperties,
+) => {
+  if (!container || document.getElementById('ive')) return
+
+  const root = document.createElement('div')
+  root.id = 'ive'
+
+  Object.entries(styles).forEach(([key, value]) => {
+    root.style[key as unknown as number] = value
   })
 
-  document.getElementById('ive')?.remove()
-
-  if (window.location.href.includes(EROSCRIPT_URL)) {
-    const root = document.createElement('div')
-    root.id = 'ive'
-    root.style.position = 'relative'
-
-    document.getElementsByClassName('icons d-header-icons')[0]?.prepend(root)
-
-    ReactDOM.createRoot(root).render(
-      <StrictMode>
-        <EroLoadPanel />
-      </StrictMode>,
-    )
-  } else if (window.location.href.includes(FAPTAP_URL)) {
-    const container = await findHtmlElement(
-      '#app > div > div.py-2.lg\\:py-8.lg\\:px-8.flex-1.flex.flex-col.relative > div.flex-1 > div.text-white.flex.flex-col.gap-y-2.mt-3.px-2.md\\:px-0 > div.flex.flex-col-reverse.lg\\:flex-row.lg\\:items-center.justify-between.gap-y-2 > div.relative.-mx-2.lg\\:mx-0 > div > div',
-    )
-
-    if (!container) {
-      return
-    }
-
-    const root = document.createElement('div')
-    root.id = 'ive'
-    root.style.position = 'relative'
-
-    container?.prepend(root)
-
-    ReactDOM.createRoot(root).render(
-      <StrictMode>
-        <FaptapPanel />
-      </StrictMode>,
-    )
-  } else if (scripts && scripts[1]) {
-    const root = document.createElement('div')
-    root.id = 'ive'
-    root.style.zIndex = '2147483640'
-    root.style.position = 'fixed'
-    root.style.inset = '0'
-    root.style.pointerEvents = 'none'
-
-    document.body.appendChild(root)
-
-    ReactDOM.createRoot(root).render(
-      <StrictMode>
-        <VideoPage scripts={scripts[1]} />
-      </StrictMode>,
-    )
+  if (insertMethod === 'prepend') {
+    container.prepend(root)
+  } else {
+    container.appendChild(root)
   }
+
+  console.log('Mounting IVE component')
+  ReactDOM.createRoot(root).render(<StrictMode>{component}</StrictMode>)
+
+  mountedComponent = true
 }
 
 // Initial setup
