20842142ef722f694f775d7314f61c8537c1ddc5
ive <contact@iveplay.io>
2025-06-28 01:05:17 +0200

    fix: scrub bug

--- src/hooks/useVideoListener.ts
+++ src/hooks/useVideoListener.ts
@@ -63,90 +63,77 @@ export const useVideoListener = (videoElement: HTMLVideoElement | null) => {
 
     // Handler for seeking
     const handleSeeking = async () => {
-      if (!videoElement.paused) {
-        setCurrentTime(videoElement.currentTime * 1000)
-        setDuration(videoElement.duration * 1000)
+      setCurrentTime(videoElement.currentTime * 1000)
+      setDuration(videoElement.duration * 1000)
 
-        try {
-          await chrome.runtime.sendMessage({
-            type: MESSAGES.PLAY,
-            timeMs: videoElement.currentTime * 1000,
-            playbackRate: videoElement.playbackRate,
-            duration: videoElement.duration * 1000,
-            loop: false,
-          })
-        } catch (error) {
-          console.error('Error syncing time:', error)
-        }
+      try {
+        await chrome.runtime.sendMessage({
+          type: MESSAGES.SEEK,
+          timeMs: videoElement.currentTime * 1000,
+        })
+      } catch (error) {
+        console.error('Error syncing time:', error)
       }
     }
 
     // Handler for rate change
     const handleRateChange = async () => {
-      if (!videoElement.paused) {
-        setCurrentTime(videoElement.currentTime * 1000)
-        setDuration(videoElement.duration * 1000)
+      setCurrentTime(videoElement.currentTime * 1000)
+      setDuration(videoElement.duration * 1000)
 
-        try {
-          await chrome.runtime.sendMessage({
-            type: MESSAGES.PLAY,
-            playbackRate: videoElement.playbackRate,
-            timeMs: videoElement.currentTime * 1000,
-            duration: videoElement.duration * 1000,
-            loop: false,
-          })
-        } catch (error) {
-          console.error('Error handling rate change:', error)
-        }
+      try {
+        await chrome.runtime.sendMessage({
+          type: MESSAGES.PLAY,
+          playbackRate: videoElement.playbackRate,
+          timeMs: videoElement.currentTime * 1000,
+          duration: videoElement.duration * 1000,
+          loop: false,
+        })
+      } catch (error) {
+        console.error('Error handling rate change:', error)
       }
     }
 
     // Handler for time update
     const handleTimeUpdate = async () => {
-      if (!videoElement.paused) {
-        setCurrentTime(videoElement.currentTime * 1000)
-        try {
-          await chrome.runtime.sendMessage({
-            type: MESSAGES.TIME_CHANGE,
-            timeMs: videoElement.currentTime * 1000,
-          })
-        } catch (error) {
-          console.error('Error syncing time:', error)
-        }
+      setCurrentTime(videoElement.currentTime * 1000)
+      try {
+        await chrome.runtime.sendMessage({
+          type: MESSAGES.TIME_CHANGE,
+          timeMs: videoElement.currentTime * 1000,
+        })
+      } catch (error) {
+        console.error('Error syncing time:', error)
       }
     }
 
     // Handler for duration change
     const handleDurationChange = async () => {
-      if (!videoElement.paused) {
-        setDuration(videoElement.duration * 1000)
+      setDuration(videoElement.duration * 1000)
 
-        try {
-          await chrome.runtime.sendMessage({
-            type: MESSAGES.DURATION_CHANGE,
-            duration: videoElement.duration * 1000,
-          })
-        } catch (error) {
-          console.error('Error handling duration change:', error)
-        }
+      try {
+        await chrome.runtime.sendMessage({
+          type: MESSAGES.DURATION_CHANGE,
+          duration: videoElement.duration * 1000,
+        })
+      } catch (error) {
+        console.error('Error handling duration change:', error)
       }
     }
 
     // Handler for volume change
     const handleVolumeChange = async () => {
-      if (!videoElement.paused) {
-        setVolume(videoElement.volume)
-        setIsMuted(videoElement.muted)
+      setVolume(videoElement.volume)
+      setIsMuted(videoElement.muted)
 
-        try {
-          await chrome.runtime.sendMessage({
-            type: MESSAGES.VOLUME_CHANGE,
-            volume: videoElement.volume,
-            muted: videoElement.muted,
-          })
-        } catch (error) {
-          console.error('Error handling volume change:', error)
-        }
+      try {
+        await chrome.runtime.sendMessage({
+          type: MESSAGES.VOLUME_CHANGE,
+          volume: videoElement.volume,
+          muted: videoElement.muted,
+        })
+      } catch (error) {
+        console.error('Error handling volume change:', error)
       }
     }
 
