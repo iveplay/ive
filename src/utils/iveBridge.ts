6cf8c0417eb3d1d905ec4bc57265ebf1e1ef8e3d
ive <contact@iveplay.io>
2025-09-02 19:32:42 +0200

    feat: reuse utils for bridge

--- src/utils/iveBridge.ts
+++ src/utils/iveBridge.ts
@@ -1,4 +1,16 @@
 import { MESSAGES } from '@background/types'
+import {
+  addToFavorites,
+  createEntry,
+  deleteEntry,
+  getAllEntries,
+  getEntry,
+  getFavorites,
+  isFavorited,
+  removeFromFavorites,
+  searchEntries,
+  updateEntry,
+} from './iveDbUtils'
 
 const ALLOWED_ORIGIN =
   process.env.NODE_ENV === 'development'
@@ -13,74 +25,51 @@ export const setupIveBridge = () => {
     if (event.source !== window) return
 
     const message = event.data
-    if (!message || message.from !== 'iveplay-page') return
+    if (!message || message.from !== 'iveplay') return
 
     try {
       let response
 
       switch (message.type) {
-        case 'IVEDB_GET_ALL_ENTRIES':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_GET_ALL_ENTRIES,
-          })
+        case MESSAGES.IVEDB_GET_ALL_ENTRIES:
+          response = await getAllEntries()
           break
 
-        case 'IVEDB_GET_ENTRY':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_GET_ENTRY,
-            entryId: message.entryId,
-          })
+        case MESSAGES.IVEDB_GET_ENTRY:
+          response = await getEntry(message.entryId)
           break
 
-        case 'IVEDB_CREATE_ENTRY':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_CREATE_ENTRY,
-            data: message.data,
-          })
+        case MESSAGES.IVEDB_CREATE_ENTRY:
+          response = await createEntry(message.data)
           break
 
-        case 'IVEDB_UPDATE_ENTRY':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_UPDATE_ENTRY,
-            entryId: message.entryId,
-            updates: message.updates,
-          })
+        case MESSAGES.IVEDB_UPDATE_ENTRY:
+          response = await updateEntry(message.entryId, message.updates)
           break
 
-        case 'IVEDB_DELETE_ENTRY':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_DELETE_ENTRY,
-            entryId: message.entryId,
-          })
+        case MESSAGES.IVEDB_DELETE_ENTRY:
+          response = await deleteEntry(message.entryId)
           break
 
-        case 'IVEDB_ADD_FAVORITE':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_ADD_FAVORITE,
-            entryId: message.entryId,
-          })
+        case MESSAGES.IVEDB_ADD_FAVORITE:
+          response = await addToFavorites(message.entryId)
           break
 
-        case 'IVEDB_REMOVE_FAVORITE':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_REMOVE_FAVORITE,
-            entryId: message.entryId,
-          })
+        case MESSAGES.IVEDB_REMOVE_FAVORITE:
+          response = await removeFromFavorites(message.entryId)
           break
 
-        case 'IVEDB_GET_FAVORITES':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_GET_FAVORITES,
-          })
+        case MESSAGES.IVEDB_GET_FAVORITES:
+          response = await getFavorites()
           break
 
-        case 'IVEDB_IS_FAVORITED':
-          response = await chrome.runtime.sendMessage({
-            type: MESSAGES.IVEDB_IS_FAVORITED,
-            entryId: message.entryId,
-          })
+        case MESSAGES.IVEDB_IS_FAVORITED:
+          response = await isFavorited(message.entryId)
           break
 
+        case MESSAGES.IVEDB_SEARCH_ENTRIES:
+          response = await searchEntries(message.options)
+          break
         default:
           throw new Error(`Unknown message type: ${message.type}`)
       }
